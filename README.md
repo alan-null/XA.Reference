bc462b7ef749283748c4139c0e9d239b29726974
Alan Płócieniak <alan-null@outlook.com>
2017-02-12 14:11:16 +0100

    Project build & publish script README

--- README.md
+++ README.md
@@ -2,3 +2,45 @@
 **SXA Reference** is an example Sitecore project build on top of **Sitecore Experience Accelerator**.
 
 The main goal of this reference project is to show **SXA** features and give guidelines on how to build/configure new sites.
+
+
+# Installation
+## Clone Repository
+Open command line and run following command
+```
+clone https://github.com/alan-null/XA.Reference.git
+```
+## Install Sitecore & SXA
+The next step is to create a new Sitecore instnace and then install SXA package.
+
+Current **XA.Reference** project is compatible with:
+
+| Product   |      Version      |  Revision |
+|----------|:-------------:|:------:|
+| Sitecore |  **8.2** | rev. 161221 |
+| SXA  |  **1.2** | rev. 161205 |
+
+
+## Environment specific configs
+
+1. Open repository root.
+2. Make a copy of following files and remove `.example` from the file name:
+   * `publishsettings.targets.example`,
+   * `zzz.Sxa.Reference.config.example`
+3. Fill file content of each file with your settings
+   * `publishsettings.targets` - set **publishUrl**. This is your site name (IIS site name),
+   * `zzz.Sxa.Reference.config` - set value of Sitecore variable named **sourceFolder**. Variable should point to the *src* folder from **XA.Reference** repository
+
+
+## Build and publish
+- Open *PowerShell Console* as **Administrator**
+- Navigate to **XA.Reference** repository root
+- Execute
+
+ ```powershell
+.\publish.ps1
+```
+
+**Alternative way:**
+
+You can build whole solution using Visual Studio and publish selected projects using *WebPublish* (hit `Alt + B + H` and select publish).
---
+++ publish.ps1
@@ -0,0 +1,50 @@
+﻿#### Configuration
+$deleteExistingFiles = $false
+$configuration  = "Debug"
+
+$publishsettings = ".\publishsettings.targets"
+$sxaReferenceConfig = ".\zzz.Sxa.Reference.config"
+####
+
+
+function Test-ConfigExists($configName){
+    if(Test-Path $configName){
+        $true
+    }else{
+        Write-Warning "Could not find config: '$($configName)"
+        Write-Warning "Make a copy of '$($currentDirectory)\$($configName).example' file. "
+        Write-Warning "Then remove '.example' from the file name and fill its content with your settings."
+        $false
+    }
+}
+
+
+if(-not (Test-ConfigExists $sxaReferenceConfig) -or -not(Test-ConfigExists $publishsettings)){
+    exit
+}
+
+[xml]$targets = Get-Content -Path $publishsettings
+$publishUrl = $targets.Project.PropertyGroup.publishUrl
+$siteName = (Split-Path $publishUrl -NoQualifier).TrimStart("\").TrimStart("/")
+
+$sxa_site = Get-Website | ? { $_.Name -eq $siteName }
+$publishPath = $sxa_site.physicalPath
+$currentDirectory = Get-Item .
+
+clear
+
+Write-Host "1. Restoring Nuget packages" -ForegroundColor "Green"
+.\nuget\nuget.exe restore .\XA.Reference.sln 
+
+Write-Host "2. Building projects" -ForegroundColor "Green"
+Get-ChildItem $currentDirectory.FullName -Recurse -Filter "*.csproj"| %{
+    $projectPath = $_.FullName.Replace($currentDirectory.FullName,".")
+    Write-Host "`tBuilding project $($_.Name)" -ForegroundColor "Cyan"
+    msbuild $projectPath /p:Configuration=$configuration /p:Platform=AnyCPU /t:WebPublish /p:WebPublishMethod=FileSystem /p:DeleteExistingFiles=$deleteExistingFiles /p:publishUrl=$publishPath /v:q
+}
+
+
+Write-Host "3. Deploying '$sxaReferenceConfig'" -ForegroundColor "Green"
+Copy-Item $sxaReferenceConfig "$($publishPath)\App_Config\Include"
+
+
